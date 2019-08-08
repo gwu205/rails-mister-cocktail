b8334bdeabe1748e75bf0b073cdb06a9261bd989
Geoffrey Wu <gwu20510@gmail.com>
2019-08-08 17:35:11 +0900

    application

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,47 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def show
+    @doses = Dose.where(id: params[:id])
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
