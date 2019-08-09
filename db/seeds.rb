5778022d84e138d31acf70651da9837e914ed4e0
Geoffrey Wu <gwu20510@gmail.com>
2019-08-09 17:23:42 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,18 @@
 
 require 'open-uri'
 require 'json'
+require 'faker'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open("#{url}").read
 ingredients = JSON.parse(ingredients_serialized)["drinks"]
 ingredients.each { |ingredient| Ingredient.create(name: ingredient["strIngredient1"]) }
+
+10.times do
+  # photo = Cloudinary::Uploader.upload("https://source.unsplash.com/1600x900/?cocktail")
+  cocktail = Cocktail.new(name: Faker::Coffee.blend_name, photo: 'https://res.cloudinary.com/gswu205/image/upload/v1565338270/zlby7tqqb9pqlpirx7m5.jpg')
+  cocktail.save!
+  5.times do
+    Dose.create(description: rand(1..4), cocktail_id: cocktail.id, ingredient_id: Ingredient.all.sample.id)
+  end
+end
