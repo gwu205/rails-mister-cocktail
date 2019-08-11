d4d783853dc8d6c02774fb47a81f97510038c69f
Geoffrey Wu <gwu20510@gmail.com>
2019-08-11 16:16:34 +0900

    updated seeds with cocktail names and img link

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,42 @@ ingredients_serialized = open("#{url}").read
 ingredients = JSON.parse(ingredients_serialized)["drinks"]
 ingredients.each { |ingredient| Ingredient.create(name: ingredient["strIngredient1"]) }
 
-10.times do
+cocktails = [
+    'Amber Moon',
+    'Blue Blazer',
+    'Black Nail',
+    'Bobby Burns',
+    'Boulevardier',
+    'Bourbon Lancer',
+    'Brooklyn',
+    'Churchill',
+    'Farnell',
+    'Irish coffee IBA',
+    'Horsefeather',
+    'Jack and Coke',
+    'Jungle Juice',
+    'Lynchburg Lemonade',
+    'Manhattan IBA',
+    'Mint Julep IBA',
+    'Missouri Mule',
+    'Nixon',
+    'Old Fashioned IBA',
+    'Rob Roy',
+    'Rusty Nail IBA',
+    'Sazerac IBA',
+    'Scotch and soda',
+    'Seven and Seven or 7 & 7',
+    'Three Wise Men',
+    'Toronto',
+    'Ward 8',
+    'Whisky Mac',
+    'Whiskey smash',
+    'Whiskey sour IBA'
+]
+
+10.times do |i|
   # photo = Cloudinary::Uploader.upload("https://source.unsplash.com/1600x900/?cocktail")
-  cocktail = Cocktail.new(name: Faker::Coffee.blend_name, remote_photo_url: 'https://res.cloudinary.com/gswu205/image/upload/v1565338270/zlby7tqqb9pqlpirx7m5.jpg')
+  cocktail = Cocktail.new(name: cocktails.delete(cocktails.sample), remote_photo_url: `https://source.unsplash.com/1600x900/?cocktail/#{i}`)
   cocktail.save!
   5.times do
     Dose.create(description: rand(1..4), cocktail_id: cocktail.id, ingredient_id: Ingredient.all.sample.id)
